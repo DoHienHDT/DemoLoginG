abe35d5c15cacd709493ff8d43368d651e3ec3e8
dohien <dohien11091999@gmail.com>
2018-09-11 15:48:59 +0700

    DemoLoginGoogle

--- DemoLoginG/AppDelegate.swift
+++ DemoLoginG/AppDelegate.swift
@@ -7,16 +7,23 @@
 //
 
 import UIKit
-
+import GoogleSignIn
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    
+
+    
 
     var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        GIDSignIn.sharedInstance().clientID = "828647423562-vr0d1v9nqoq801vk1t2uk2fv7v1nl5as.apps.googleusercontent.com"
         return true
+    }
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        return GIDSignIn.sharedInstance().handle(url as URL?, sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as? String, annotation: options[UIApplicationOpenURLOptionsKey.annotation])
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
--- DemoLoginG/ViewController.swift
+++ DemoLoginG/ViewController.swift
@@ -7,19 +7,50 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import GoogleSignIn
+class ViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate{
+    
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var photoImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().delegate = self
+        
+        let googleSignInButton = GIDSignInButton()
+        googleSignInButton.center = view.center
+
+        view.addSubview(googleSignInButton)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if error != nil {
+            print(error ?? "google error")
+            return
+        }
+        emailLabel.text = user.profile.email
+        photoImage.image = UIImage(url: user.profile.imageURL(withDimension: 400))
+        GIDSignIn.sharedInstance().signOut()
+        print(user.profile.imageURL(withDimension: 400))
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+  
 
 }
-
+extension UIImage {
+    convenience init?(url: URL?) {
+        guard let url = url else { return nil }
+        do {
+            let data = try Data(contentsOf: url)
+            self.init(data: data)
+        } catch {
+            print("Cannot load image from url: \(url) with error: \(error)")
+            return nil
+        }
+    }
+}
